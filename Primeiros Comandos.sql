01daf8dcdf16e20bfa8644948db258ed3456f0cd
Dev Organizada <devorganizada@gmail.com>
2022-02-22 13:43:20 -0300

    Inserindo dados na tabela

--- Primeiros Comandos.sql
+++ Primeiros Comandos.sql
@@ -6,3 +6,10 @@ create table ideias(
        id_ideias int not null constraints pk_ideia primary key,
        tema varchar2(200) not null
 );
+
+--Inserindo dados na tabela
+insert into ideias(id_ideias,tema) values(1,'Comandos básicos no sql');
+insert into ideias values(2,'Usando mais o select');
+insert into ideias values(3,'Criando uma foreing key');
+insert into ideias values(4,'Fazendo join entre tabelas');
+commit;
