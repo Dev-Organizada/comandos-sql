c8501803315d3407db8bc4e5c6ee4a139fe4e64d
Dev Organizada <devorganizada@gmail.com>
2022-02-22 13:38:55 -0300

    Comando para criação de tabela

--- Primeiros Comandos.sql
+++ Primeiros Comandos.sql
@@ -1,2 +1,8 @@
 Comandos Básicos em SQL
 --Criação de tabelas
+
+--Criação de tabela para armazenar ideias de postagem
+create table ideias(
+       id_ideias int not null constraints pk_ideia primary key,
+       tema varchar2(200) not null
+);
